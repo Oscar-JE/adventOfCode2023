6a054a3d4b29e340dbde8b6546569f8d4bfc6b59
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2023-12-17 14:31:22 +0100

    part1 klar

--- day8/main.go
+++ day8/main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"bufio"
+	"day8/graph"
+	"day8/traverser"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func main() {
-	// parse
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic("file not found")
+	}
+	scan := bufio.NewScanner(file)
+	scan.Scan()
+	policyLine := scan.Text()
+	scan.Scan() // we know whis is a blank row
+	graphInfo := []graph.NameLeftRight{}
+	for scan.Scan() {
+		nodeLine := scan.Text()
+		splited := strings.Split(nodeLine, "=")
+		name := strings.Trim(splited[0], " ")
+		cleaned := strings.Trim(splited[1], "() ")
+		leftAndRight := strings.Split(cleaned, ", ")
+		graphInfo = append(graphInfo, graph.NameLeftRight{Name: name, Left: leftAndRight[0], Right: leftAndRight[1]})
+	}
+	trav := traverser.Init(policyLine, graphInfo)
+	fmt.Println(trav.NrOfSteps())
 }
