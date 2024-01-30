0605471dfb5e9e2803cfed720225733130c4cd23
Oscar Johansson Elling <oscar.johansson.elling@gmail.com>
2024-01-30 19:16:53 +0100

    städa undant sådant vi inte vet är användbart

--- day8/congruence/loop.go
+++ day8/congruence/loop.go
@@ -1,10 +1,5 @@
 package congruence
 
-/*
-	I den här filen undersöker congruence problem och hur man löser dem.
-	Vi börjar i svansen och rör oss sedan in i loopen
-*/
-
 type TailedLoopMultipleEndPoints struct {
 	tail         int
 	loopLength   int
@@ -12,26 +7,5 @@ type TailedLoopMultipleEndPoints struct {
 }
 
 func InitTailedLoop(tail int, loopLength int, winningIndexes []int) TailedLoopMultipleEndPoints {
-	if someNegative(append([]int{tail, loopLength}, winningIndexes...)) {
-		panic("negative integers not allowed in congruence problem")
-	}
 	return TailedLoopMultipleEndPoints{tail: tail, loopLength: loopLength, winingPoints: winningIndexes}
-}
-
-func someNegative(numbers []int) bool {
-	negative := false
-	for _, nr := range numbers {
-		negative = negative || nr < 0
-	}
-	return negative
-}
-
-type TailedLoop struct {
-	tail         int
-	loopLength   int
-	winningPoint int
-}
-
-func cut(loop1 TailedLoop, loop2 TailedLoop) (TailedLoop, error) {
-	return loop2, nil
 }
