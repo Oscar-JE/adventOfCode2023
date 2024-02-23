1c3c38a912e07be4c8cbc3d3508d339d80995c7d
viojog <oscar.johanssonelling@vitecsoftware.com>
2024-02-23 10:19:31 +0100

    pill under mötet

--- day8/congruence/loop.go
+++ day8/congruence/loop.go
@@ -3,9 +3,17 @@ package congruence
 type TailedLoopMultipleEndPoints struct {
 	tail         int
 	loopLength   int
-	winingPoints []int
+	winingPoints []int // meh hur gör om de finns på svansen? Kanske inte krävs av uppgiften eller så är 0 från början av svansen. Ja så gör vi
 }
 
 func InitTailedLoop(tail int, loopLength int, winningIndexes []int) TailedLoopMultipleEndPoints {
 	return TailedLoopMultipleEndPoints{tail: tail, loopLength: loopLength, winingPoints: winningIndexes}
+}
+
+func (loop TailedLoopMultipleEndPoints) Eq(other TailedLoopMultipleEndPoints) bool {
+	taledEq := loop.tail == other.tail
+	loopLenEq := loop.loopLength == other.loopLength
+	if !taledEq || !loopLenEq {
+		return false;
+	}
 }
--- day8/traverser/traverser_test.go
+++ day8/traverser/traverser_test.go
@@ -3,6 +3,7 @@ package traverser
 import (
 	"day8/graph"
 	"day8/policy"
+	"day8/congruence"
 	"testing"
 )
 
@@ -18,6 +19,13 @@ func TestFinndLoop(t *testing.T) {
 	pol := policy.Init("LLR")
 	loop := findLoop(pol, example)
 	// AAA 0 , BBB 1, AAA 2 , BBB 0, AAA 1, BBB 2, ZZZ 0, ZZZ 1 , ZZZ 2  , ZZZ 0 hittatloop
+	winingPoints := []int{6,7,8}
+	tailLength := 6
+	loopLength := 3
+	expectedLoop := congruence.InitTailedLoop(tailLength,loopLength, winingPoints)
+	if (expectedLoop!= loop){
+		t.Errorf("unexpected loop")
+	}
 }
 
 func simple2() []graph.NameLeftRight {
