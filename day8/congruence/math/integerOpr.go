2e25ec0f7eccf3d221d629bd2e22c79bc2ed3362
oscar johansson elling <oscar.johansson.elling@gmail.com>
2024-03-21 20:50:27 +0100

    klart för idag

--- day8/congruence/math/integerOpr.go
+++ day8/congruence/math/integerOpr.go
@@ -8,7 +8,5 @@ func Max(a int, b int) int {
 }
 
 func GDC(a int, b int) int {
-	return 5 // hur gör man deta snyggt ?
-	// isprime skulle vara snyggt men det skulle också bli dyrt
-	// om man inte kan göra isprime till ett slutsteg för rekursionen
+	return 5
 }
