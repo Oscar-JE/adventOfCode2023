5328a9215ef870c08306e46edc91993944d1241a
Oscar Johannsson Elling <oscar.johansson.elling@gmail.com>
2024-05-11 10:31:46 +0200

    en positiv riktning på puzzelbitarna

--- day12/order/order.go
+++ day12/order/order.go
@@ -41,20 +41,32 @@ func (o *Order) Unfold(times int) {
 	}
 }
 
-func (o Order) ExpandLeft(nr int) Order {
+func (o Order) RollBeginning(startIndex int) Order {
 	res := o.ordering
-	for i := 0; i < nr; i++ {
-		j := len(o.ordering) - 1 - i%(len(o.ordering))
-		res = append([]int{o.ordering[j]}, res...)
+	if startIndex < 0 {
+		for i := -1; i >= startIndex; i-- {
+			j := (i + 10*o.Len()) % o.Len()
+			res = append([]int{o.ordering[j]}, res...)
+		}
+	} else if 0 <= startIndex && startIndex < o.Len() {
+		res = res[startIndex:]
+	} else {
+		res = []int{}
 	}
 	return Order{ordering: res}
 }
 
-func (o Order) ExpandRight(nr int) Order { // kräver test dessa två
+func (o Order) RollEnd(nr int) Order {
 	res := o.ordering
-	for i := 0; i < nr; i++ {
-		j := i % len(o.ordering)
-		res = append(res, o.ordering[j])
+	if nr <= -o.Len() {
+		res = []int{}
+	} else if -o.Len() < nr && nr < 0 {
+		res = o.ordering[:o.Len()+nr]
+	} else {
+		for i := range nr {
+			j := i % len(o.ordering)
+			res = append(res, o.ordering[j])
+		}
 	}
 	return Order{ordering: res}
 }
--- day12/order/order_test.go
+++ day12/order/order_test.go
@@ -2,6 +2,38 @@ package order
 
 import "testing"
 
-func TestExandLeft(t *testing.T) {
-	t.Errorf("contiu here")
+func TestRollBeginningHappy(t *testing.T) {
+	order := Init([]int{1, 2, 3})
+	expected := Init([]int{3})
+	res := order.RollBeginning(2)
+	if !res.Eq(expected) {
+		t.Errorf("review roll beginning for order")
+	}
+}
+
+func TestRollBegginingNegativ(t *testing.T) {
+	order := Init([]int{1, 2, 3})
+	expecteed := Init([]int{3, 1, 2, 3, 1, 2, 3})
+	res := order.RollBeginning(-4)
+	if !res.Eq(expecteed) {
+		t.Errorf("roll left beter sig konstigt")
+	}
+}
+
+func TestRollEnd(t *testing.T) {
+	order := Init([]int{1, 2, 3})
+	expected := Init([]int{1, 2, 3, 1, 2, 3, 1})
+	res := order.RollEnd(4)
+	if !res.Eq(expected) {
+		t.Errorf("review roll end for order")
+	}
+}
+
+func TestRollEndNegative(t *testing.T) {
+	order := Init([]int{1, 2, 3})
+	expected := Init([]int{1})
+	res := order.RollEnd(-2)
+	if !res.Eq(expected) {
+		t.Errorf("review expand right for order")
+	}
 }
