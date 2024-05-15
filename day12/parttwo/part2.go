7bde022a57cdfad3e6709b4845743ea193f73947
viojog <oscar.johanssonelling@vitecsoftware.com>
2024-05-15 12:38:49 +0200

    funderingar

--- day12/parttwo/part2.go
+++ day12/parttwo/part2.go
@@ -11,3 +11,20 @@ func NumberOfCombination(f field.Field, o order.Order) int {
 	o.Unfold(4)
 	return partone.NumberOfCombination(f, o)
 }
+
+func NumberOfCombinations2(f field.Field, o order.Order, repetitions int) int {
+	o.Unfold(repetitions - 1)
+	return positionOfNumbers(f, o, repetitions)
+}
+
+func positionOfNumbers(f field.Field, o order.Order, depth int) int {
+	possibleOrderConsumptionsAndDemandsOnNext := []FieldOrderAndNrOfSolutions{}
+	return len(possibleOrderConsumptionsAndDemandsOnNext)
+}
+
+type FieldOrderAndNrOfSolutions struct {
+	f           field.Field // behövs detta fält sparas? svar egentligen inte
+	o           order.Order
+	demand      int // -1 , flat , 0 # middle next need to be '.' , 1 M:# f:#, s:'.' och så vidare
+	nrSolutions int // enbart en cashning
+}
