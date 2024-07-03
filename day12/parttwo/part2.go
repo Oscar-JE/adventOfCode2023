1dd661337c2baef8ff376e2335573f960a4102f0
Oscar Johannsson Elling <oscar.johansson.elling@gmail.com>
2024-07-03 22:46:25 +0200

    öppna för att byta dator

--- day12/parttwo/part2.go
+++ day12/parttwo/part2.go
@@ -15,17 +15,9 @@ func SolvePart1Varant(f field.Field, s solution.SolutionKey) int {
 	return partone.NumberOfCombination(f, s.SubOrder)
 }
 
-func FindAllPossibleNextkeysMiddle(previous solution.SubProblem, remainingOrder order.Order) []solution.SubProblem {
-	// här måste vi definiera hur pusselbitarna ska sättas ihop
-	// behöver en typ som inte är solutionkey utan subProblem eller liknande
-	// det bör finnas ett enklare sätt att få till detta
-	var nextSubproblems []solution.SubProblem = []solution.SubProblem{}
-	return nextSubproblems
-}
-
 func NumberOfCombination(f field.Field, o order.Order, dept int) int {
-	o.Unfold(dept)                                               // hur många gånger det skulle upprepas
-	original := generation{solution.NoConnectionDDemand(), o, 1} // det här kanske fungerar
+	o.Unfold(dept)
+	original := generation{solution.NoConnectionDDemand(), o, 1}
 	childrenOfTime := []generation{original}
 	for i := 0; i < dept; i++ {
 		nextChildren := nextGeneration(childrenOfTime, f)
@@ -46,4 +38,8 @@ type generation struct {
 	prevRightConnection solution.Connection
 	remainingOrder      order.Order
 	nrSolutions         int
+}
+
+func (g generation) branchOut(f field.Field) {
+
 }
