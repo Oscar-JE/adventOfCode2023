52661bc4c6695fb95a1f0092ecb2ae8c5b9dd105
oscar johansson elling <oscar.johansson.elling@gmail.com>
2024-11-04 22:04:02 +0100

    bygg upp ett sett object för items

--- day19/set/interval.go
+++ day19/set/interval.go
@@ -5,12 +5,33 @@ type Interval struct {
 	upper int
 }
 
+func (i Interval) Cardinality() int {
+	return max(i.upper-i.lower, 0)
+}
+
+func (i Interval) DemandAbove(limit int) Interval {
+	return Interval{i.lower, min(limit, i.upper)}
+}
+
+func (i Interval) DemandBelow(limit int) Interval {
+	newLowerBond := max(i.lower, limit+1)
+	return Interval{newLowerBond, i.upper}
+}
+
+func (i Interval) IsEmpty() bool {
+	return i.Cardinality() == 0
+}
+
 func Intersect(a Interval, b Interval) Interval {
 	return Interval{lower: max(a.lower, b.lower), upper: min(a.upper, b.upper)}
 }
 
 func (i Interval) contains(other Interval) bool {
+	return i.lower <= other.lower && other.upper <= i.upper
+}
 
+func (i Interval) containsElement(el int) bool {
+	return i.lower <= el && el < i.upper
 }
 
 func (i Interval) Remove(other Interval) []Interval {
