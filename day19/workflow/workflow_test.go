ae572e2556d1a689b9c70679415a1d41b6357057
oscar johansson elling <oscar.johansson.elling@gmail.com>
2024-11-10 18:22:28 +0100

    enkelt test inte tillräckligt upplysande

--- day19/workflow/workflow_test.go
+++ day19/workflow/workflow_test.go
@@ -6,11 +6,29 @@ import (
 	"testing"
 )
 
-func TestInit(t *testing.T) { // kan väl räcka med den nivån
+func TestSimpleWorkflowNoAccepted(t *testing.T) {
 	nodes := []NodeInfo{}
-	nodes = append(nodes, NodeInfo{Id: "in", Rules: []RuleRep{{Demand: item.LessMusical{Musical: 4}, DestinationId: "A"}, {Demand: item.NoDemand{}, DestinationId: "R"}}})
+	nodes = append(nodes, NodeInfo{Id: "in", Rules: []RuleRep{{Demand: item.LessMusical{Musical: 1}, DestinationId: "A"}, {Demand: item.NoDemand{}, DestinationId: "R"}}})
 	nodes = append(nodes, NodeInfo{Id: "A", Rules: []RuleRep{}})
 	nodes = append(nodes, NodeInfo{Id: "R", Rules: []RuleRep{}})
 	workFlow := Init(nodes)
+	res := workFlow.FindNumberOfPossibleAccept()
+	if res != 0 {
+		t.Errorf("impossible demans should result in zero combination")
+	}
+	fmt.Println(workFlow)
+}
+
+func TestSimpleWorkflowOneAccepted(t *testing.T) {
+	nodes := []NodeInfo{}
+	nodes = append(nodes, NodeInfo{Id: "in", Rules: []RuleRep{{Demand: item.LessMusical{Musical: 2}, DestinationId: "A"}, {Demand: item.NoDemand{}, DestinationId: "R"}}})
+	nodes = append(nodes, NodeInfo{Id: "A", Rules: []RuleRep{}})
+	nodes = append(nodes, NodeInfo{Id: "R", Rules: []RuleRep{}})
+	workFlow := Init(nodes)
+	res := workFlow.FindNumberOfPossibleAccept()
+	expected := 4000 * 4000 * 4000 * 1
+	if res != expected {
+		t.Errorf("expected: %d but gott %d", expected, res)
+	}
 	fmt.Println(workFlow)
 }
